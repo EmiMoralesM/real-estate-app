dffc7c3586f2710d4f69737f8253251a1da86882
Emiliano Morales <111534564+EmiMoralesM@users.noreply.github.com>
2023-07-11 19:14:08 -0300

    Day 1
    - App creation
    - Data fetched and tested

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [properties, setProperties] = useState()
+
+  useEffect(() => {
+    fetch("http://localhost:4500/api/")
+      .then(res => res.json())
+      .then((data) => setProperties(data))
+      .catch(err => console.log(`Error: ${err}`))
+  }, [])
+
+  return (
+    <>
+      {(!properties) ? (
+        <p>Loading...</p>
+      ) : (
+        properties.map((property, i) => (
+          <div key={i}>
+            <img src={property.images[0]} width='200px' alt="" />
+            <h1>${property.price}</h1>
+            <h3>{property.address}</h3>
+            <br />
+            <br />
+            <br />
+          </div>
+        ))
+      )}
+    </>
+  )
+}
+
+export default App
