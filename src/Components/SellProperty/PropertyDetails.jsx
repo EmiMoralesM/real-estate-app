a1804ed17854ec14b81972c200458ed15b81e162
Emiliano Morales <111534564+EmiMoralesM@users.noreply.github.com>
2023-07-24 03:23:47 -0300

    Day 13
    - Sell property
        - Property Details (100%)
            - Image load

--- src/Components/SellProperty/PropertyDetails.jsx
+++ src/Components/SellProperty/PropertyDetails.jsx
@@ -1,49 +1,117 @@
 import React, { useContext, useState } from 'react'
 import { Context } from '../../assets/Context'
+import test from '../../assets/imgs/banner.jpg'
 
 function PropertyDetails() {
     const { useOutsideClick } = useContext(Context)
     let address = '5145 S Hatch Drive, Evergreen, CO 80439'
     const hometypes_array = ['Houses', 'Townhomes', 'Multy-family', 'Condos', 'Aparments']
 
+    
     const [homeType, setHomeType] = useState(hometypes_array[0])
-    const [homeTypeOpen, setHomeTypeOpen] = useState(false)
-
     const [beds, setBeds] = useState(0)
     const [baths, setBaths] = useState(0)
     const [size, setSize] = useState(0)
     const [sizeScale, setSizeScale] = useState('Sqft')
-    const [sizeScaleOpen, setSizeScaleOpen] = useState(false)
+    
 
     const refHomeType = useOutsideClick(() => setHomeTypeOpen(false))
     const refSizeScale = useOutsideClick(() => setSizeScaleOpen(false))
+    
+    const [homeTypeOpen, setHomeTypeOpen] = useState(false)
+    const [sizeScaleOpen, setSizeScaleOpen] = useState(false)
+
+    const [mainImagePreview, setMainImagePreview] = useState('')
+    const [otherImagePreview, setOtherImagePreview] = useState([])
+
+
+
+    const previewImage = (e, type) => {
+        const reader = new FileReader()
+        if (type == 'main') {
+            reader.readAsDataURL(e.target.files[0])
+            reader.onload = () => {
+                setMainImagePreview(reader.result)
+            }
+        } else if (type == 'other') {
+            if (e.target.files.length == 1) {
+                console.log('one image');
+                reader.readAsDataURL(e.target.files[0])
+                reader.onload = () => {
+                    setOtherImagePreview(prevOtherImagePreview => [...prevOtherImagePreview, reader.result])
+                }
+            } else if (e.target.files.length > 1) {
+                for (let x = 0; x < e.target.files.length; x++) {
+                    const reader = new FileReader()
+                    reader.readAsDataURL(e.target.files[x])
+                    reader.onload = () => {
+                        setOtherImagePreview(prevOtherImagePreview => [...prevOtherImagePreview, reader.result])
+                    }
+                }
+            }
+        }
+    }
 
     return (
         <>
             <form action="#" className='propDetailsForm'>
                 <div className='propPriceDiv'>
-                    <p className='propDetailsTitle'>Price</p>
+                    <h2 className='propInfoTitle'>Price <span className='requiredField'>*</span></h2>
                     <div>
                         <span>$</span>
                         <input type="number" />
                     </div>
                     <hr />
                 </div>
-                {/* <div>
-                    <p className='propDetailsTitle'>Pictures</p>
+                <div>
+                    <h2 className='propInfoTitle'>Pictures</h2>
                     <div>
-                        <label htmlFor="">Main Image</label>
-                        <input type="file" name="" id="" accept="image/png, image/jpeg" />
+                        <p>Main Image <span className='requiredField'>*</span></p>
+                        <div className='imageContainer'>
+                            <label htmlFor="mainImage" className='picturesLabel' >
+                                <div>
+                                    <span></span>
+                                    <p>Select the main image for your product</p>
+                                </div>
+                                {!mainImagePreview && <p>Add Image</p>}
+                                {mainImagePreview && <p>Change Image</p>}
+                            </label>
+                            {mainImagePreview && <figure>
+                                <img src={mainImagePreview} id='mainImagePreview' alt="" />
+                            </figure>}
+                        </div>
+                        <input type="file" name="" onChange={(e) => previewImage(e, 'main')} id="mainImage" accept="image/png, image/jpeg" className='pictureInput' />
                     </div>
                     <div>
-                        <label htmlFor="">Other Images</label>
-                        <input type="file" name="" id="" accept="image/png, image/jpeg" />
+                        <p>Other Images <span className='requiredField'>*</span></p>
+                        <div className='imageContainer'>
+                            <label htmlFor="otherImages" className={`picturesLabel ${otherImagePreview.length > 0 ? 'picturesLabelShrink' : ''}`}>
+                                <div>
+                                    <span></span>
+                                    <p>Select other images for your product</p>
+                                </div>
+                                <p>Add Images</p>
+                            </label>
+                            {otherImagePreview.length > 0 && <figure className='otherImagesFigure'>
+                                {otherImagePreview.map((image, i) => (
+                                    <div>
+                                        <img src={image} key={i} alt="" />
+                                        <span
+                                            className='removeImage'
+                                            onClick={() => setOtherImagePreview(prevOtherImagePreview => prevOtherImagePreview.filter(item => image != item))}
+                                        ></span>
+                                    </div>
+                                ))}
+                            </figure>}
+                        </div>
+                        <input type="file" name="" multiple onChange={(e) => previewImage(e, 'other')} id="otherImages" accept="image/png, image/jpeg" className='pictureInput' />
                     </div>
-                </div> */}
+                    <hr />
+                </div>
                 <div className='propInfoDiv'>
-                    <p className='propInfoTitle'>Home Details</p>
+                    <h2 className='propInfoTitle'>Home Details</h2>
                     <div className='propInfoHomeType' ref={homeTypeOpen ? refHomeType : null}>
-                        <label htmlFor="homeType">Home Type</label>
+                        <label htmlFor="homeType">Home Type <span className='requiredField'>*</span></label>
                         <div className='homeTypeInputDiv' onClick={() => setHomeTypeOpen(prevMinPriceOpen => !prevMinPriceOpen)}>
                             <input type="text" name='homeType' id='homeType' value={homeType} readOnly />
                             <p className={`arrow ${homeTypeOpen ? 'arrowActive' : ''}`}></p>
@@ -64,16 +132,16 @@ function PropertyDetails() {
                         }
                     </div>
                     <div>
-                        <label htmlFor="beds">Beds</label>
+                        <label htmlFor="beds">Bedrooms <span className='requiredField'>*</span></label>
                         <input type="number" name="beds" id="beds" value={beds} max="99" onChange={(e) => setBeds(e.target.value)} />
                     </div>
                     <div>
-                        <label htmlFor="baths">Baths</label>
+                        <label htmlFor="baths">Baths <span className='requiredField'>*</span></label>
                         <input type="number" name="baths" id="baths" value={baths} max="99" onChange={(e) => setBaths(e.target.value)} />
                     </div>
-                    <div className='propInfoSqftDiv'>
+                    <div className='prop50PercentDiv propInfoSqftDiv'>
                         <div>
-                            <label htmlFor="size">Lot Size</label>
+                            <label htmlFor="size">Lot Size <span className='requiredField'>*</span></label>
                             <input type="number" name="size" id="size" value={size} max="99" onChange={(e) => setSize(e.target.value)} />
                         </div>
                         <div ref={sizeScaleOpen ? refSizeScale : null}>
@@ -87,9 +155,33 @@ function PropertyDetails() {
                             }
                         </div>
                     </div>
+                    <hr />
                 </div>
-
-            </form>
+                <div className='propInfoDiv'>
+                    <h2 className='propInfoTitle'>Aditional Information</h2>
+                    <div className='prop50PercentDiv'>
+                        <div>
+                            <label htmlFor="size">Related Website</label>
+                            <input type="text" />
+                        </div>
+                        <div>
+                            <label htmlFor="size">What I love about this home</label>
+                            <textarea name="" id="" cols="30" rows="7"></textarea>
+                        </div>
+                    </div>
+                    <hr />
+                </div>
+                <div className='propInfoDiv'>
+                    <h2 className='propInfoTitle'>Contact Information</h2>
+                    <p>Potential buyers will contact you through the email address you use to register on Zillow. You can also add your phone number.</p>
+                    <div>
+                        <label htmlFor="size">Phone Number</label>
+                        <input type="tel" id="phone" name="phone"/>
+                    </div>
+                    <hr />
+                </div>
+                <button className='button submitSellPorpertyButton'>Submit</button>
+            </form >
         </>
     )
 }
